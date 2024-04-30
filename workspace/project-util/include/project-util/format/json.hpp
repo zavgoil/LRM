1db6b87169ddd739a7d20ae71e9c97ace82e2699
Ilya Zavgorodniy <i.zavgorodnii@rtc.ru>
2024-04-30 20:12:24 +0300

    Добавил (де)сериализатор json

---
+++ workspace/project-util/include/project-util/format/json.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <fstream>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
+template <typename T>
+class JsonFormat {
+ public:
+  static inline std::string to_json(const T& obj) {
+    json json_data = obj;
+    return json_data.dump();
+  }
+  static inline void from_json(T& obj, const std::string& json_str) {
+    json json_data = json::parse(json_str);
+    obj = json_data.get<T>();
+  }
+  static inline void to_json_file(const T& obj, const std::string& file_path) {
+    std::ofstream file(file_path);
+    json json_data = obj;
+    file << json_data.dump(4);
+    file.close();
+  }
+  static inline void from_json_file(T& obj, const std::string& file_path) {
+    std::ifstream file(file_path);
+    json json_data = json::parse(file);
+    file.close();
+    obj = json_data.get<T>();
+  }
+
+  virtual ~JsonFormat() = default;
+  inline std::string to_json() { return to_json(dynamic_cast<T&>(*this)); }
+  inline void from_json(const std::string& json) {
+    return from_json(dynamic_cast<T&>(*this), json);
+  }
+  inline void to_json_file(const std::string& file_path) {
+    return to_json_file(dynamic_cast<T&>(*this), file_path);
+  }
+  inline void from_json_file(const std::string& file_path) {
+    return from_json_file(dynamic_cast<T&>(*this), file_path);
+  }
+};
