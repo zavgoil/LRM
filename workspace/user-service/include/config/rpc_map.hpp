9baa90b211c6275f8252935143fd6731c953d4b8
Ilya Zavgorodniy <i.zavgorodnii@rtc.ru>
2024-05-01 15:07:04 +0300

    Добавил определение пути до rpc_map через CMake

--- workspace/user-service/include/config/rpc_map.hpp
+++ workspace/user-service/include/config/rpc_map.hpp
@@ -4,6 +4,10 @@
 #include <project-util/format/yaml_macro.hpp>
 #include <string>
 
+#ifndef RPC_MAP_PATH
+#define RPC_MAP_PATH ""
+#endif
+
 struct RpcAddress : public YamlFormat<RpcAddress> {
   std::string uri = "";
   int port = 0;
