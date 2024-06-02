ab9f467122192486b3599bce7f6af2d313b6a34b
Ilya Zavgorodniy <i.zavgorodnii@rtc.ru>
2024-06-02 23:10:46 +0300

    Добавил создание модели списка Контактов

--- workspace/gui-client/src/main.cpp
+++ workspace/gui-client/src/main.cpp
@@ -3,15 +3,18 @@
 #include <QQmlContext>
 #include <QQuickStyle>
 #include <app-backend/app-backend.hpp>
+#include <app-backend/contact_list_model.hpp>
 
 int main(int argc, char *argv[]) {
   QGuiApplication app(argc, argv);
   AppBackend app_backend(argc, argv);
+  ContactListModel contact_model(&app_backend);
 
   QQmlApplicationEngine engine;
   QQuickStyle::setStyle("Material");
 
   engine.rootContext()->setContextProperty("AppBackend", &app_backend);
+  engine.rootContext()->setContextProperty("ContactModel", &contact_model);
 
   QObject::connect(
       &engine, &QQmlApplicationEngine::objectCreationFailed, &app,
