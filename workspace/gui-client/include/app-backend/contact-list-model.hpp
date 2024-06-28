1d603665bd7551c2f07365da833ab88e41fb8e42
Ilya Zavgorodniy <i.zavgorodnii@rtc.ru>
2024-06-28 22:24:08 +0300

    Косметика

--- workspace/gui-client/include/app-backend/contact-list-model.hpp
+++ workspace/gui-client/include/app-backend/contact-list-model.hpp
@@ -14,12 +14,13 @@ class ContactListModel : public QAbstractListModel {
   static const int FIRST_ROLE_INDEX = Qt::UserRole + 1;
   enum ModelRoles { TYPE = FIRST_ROLE_INDEX, DATA, END_INDICATOR };
   const inline static std::map<::user_service::NotificationClientType,
-                               std::string> TypeToString = {
-      {::user_service::NotificationClientType::INTERNAL,
-       "Внутреннее приложение"},
-      {::user_service::NotificationClientType::EMAIL, "Email"},
-      {::user_service::NotificationClientType::TELEGRAM, "Телеграм"},
-    };
+                               std::string>
+      TypeToString = {
+          {::user_service::NotificationClientType::INTERNAL,
+           "Внутреннее приложение"},
+          {::user_service::NotificationClientType::EMAIL, "Email"},
+          {::user_service::NotificationClientType::TELEGRAM, "Телеграм"},
+      };
 
   Q_INVOKABLE void addEmptyRow() {
     beginInsertRows(QModelIndex(), contacts_.size(), contacts_.size());
@@ -40,13 +41,13 @@ class ContactListModel : public QAbstractListModel {
 
   Q_INVOKABLE void editRow(int row_index, int type_index, QString data) {
     if (row_index < 0 || row_index >= contacts_.size()) return;
-    qDebug() << "EDIT";
     contacts_.at(row_index).set_type(getContactTypeByIndex(type_index));
     contacts_.at(row_index).set_data(data.toStdString());
   }
 
-  void setContacts(::google::protobuf::RepeatedPtrField<
-                   ::user_service::NotificationClient>&& contacts) {
+  void setContacts(
+      ::google::protobuf::RepeatedPtrField<::user_service::NotificationClient>&&
+          contacts) {
     if (contacts_.size() > 0) {
       beginRemoveRows(QModelIndex(), 0, contacts_.size() - 1);
       contacts_.Clear();
@@ -60,10 +61,7 @@ class ContactListModel : public QAbstractListModel {
     }
   }
 
-  Q_INVOKABLE void clearCache()
-  {
-    contacts_.Clear();
-  }
+  Q_INVOKABLE void clearCache() { contacts_.Clear(); }
 
   const ::google::protobuf::RepeatedPtrField<
       ::user_service::NotificationClient>&
@@ -104,13 +102,12 @@ class ContactListModel : public QAbstractListModel {
   }
 
  private:
-  ::google::protobuf::RepeatedPtrField< ::user_service::NotificationClient>
+  ::google::protobuf::RepeatedPtrField<::user_service::NotificationClient>
       contacts_ = {};
 
   static QStringList contactTypesToStringList() {
     QStringList list;
-    for (auto& i : TypeToString)
-      list << QString::fromStdString(i.second);
+    for (auto& i : TypeToString) list << QString::fromStdString(i.second);
     return list;
   }
 
